8302615cffeca78af9a6eab96f79af49a4ac86c3
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2023-03-03 22:02:25 -0300

    ReactiveForms
    Estudo de Formulários Reativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /** Pipes */
 import { FormatCurrencyPipe } from './shared/pipes/formatCurrency.pipe';
@@ -53,7 +53,7 @@ import { FormularioComponent } from './components/formulario/formulario.componen
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [
     {
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { OrdemDeCompraService } from 'src/app/service/ordem-de-compra.service';
 import { Pedido } from '../../shared/model/pedido.model'
 
@@ -10,11 +10,14 @@ import { Pedido } from '../../shared/model/pedido.model'
 })
 export class FormularioComponent implements OnInit {
 
-  @ViewChild('formulario') formulario!:NgForm
+  formulario: FormGroup = new FormGroup({
+    "endereco": new FormControl(null, [Validators.minLength(5), Validators.maxLength(120), Validators.required]),
+    "numero": new FormControl(null, [Validators.minLength(1), Validators.maxLength(20), Validators.required]),
+    "complemento": new FormControl(null),
+    "formaDePagamento": new FormControl(null, [Validators.required]),
+  })
 
-  @Input()
-  idPedidoCompra!: number
-
+    idPedidoCompra!: number
   constructor(
     private ordemDeCompraService: OrdemDeCompraService
     ) { }
@@ -23,17 +26,29 @@ export class FormularioComponent implements OnInit {
   }
 
   confirmarCompra(): void{
-    let pedido: Pedido = new Pedido (
-      this.formulario.value.endereco,
-      this.formulario.value.numero,
-      this.formulario.value.complemento,
-      this.formulario.value.formaDePagamento
-    )
 
-    this.ordemDeCompraService.efetivarCompra(pedido)
-    .subscribe((idPedido:number)=>{
-      this.idPedidoCompra = idPedido
-    })
+    if(this.formulario.status === 'INVALID'){
+
+      console.log('formulario está invalido')
+
+      this.formulario.get('endereco')?.markAsTouched()
+      this.formulario.get('numero')?.markAsTouched()
+      this.formulario.get('complemento')?.markAsTouched()
+      this.formulario.get('formaDePagamento')?.markAsTouched()
+    }else{
+
+      let pedido: Pedido = new Pedido(
+          this.formulario.value.endereco,
+          this.formulario.value.numero,
+          this.formulario.value.complemento,
+          this.formulario.value.formaDePagamento
+        )
+
+        this.ordemDeCompraService.efetivarCompra(pedido)
+        .subscribe((idPedido:number)=>{
+          this.idPedidoCompra = idPedido
+        })
+    }
   }
 
 }
